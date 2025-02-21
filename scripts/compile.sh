c0b0449b3c7ecd264a809622a2f3770b28a0bb12
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2025-02-21 14:49:00 -0500

    simplified nvcc compilation command

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,23 +5,18 @@ nvcc -std=c++11 \
 	../cuda/CalDTWDistMatrixMNSigroot.cu \
 	-Xcompiler \
 	-fopenmp \
-	-arch=sm_86 \
-	-gencode=arch=compute_86,code=sm_86
+	-arch=sm_86
 
 nvcc -std=c++11 \
 	-o ../bin/CalDTWDistMatrixMN \
 	../cuda/CalDTWDistMatrixMN.cu \
 	-Xcompiler \
-	-fopenmp \
-	-arch=sm_86 \
-	-gencode=arch=compute_86,code=sm_86
+	-fopenmp
 
 nvcc -std=c++11 \
 	-o ../bin/FpsCudaDTWThreshold \
 	../cuda/FpsCudaDTWThreshold.cu \
 	-Xcompiler \
-	-fopenmp \
-	-arch=sm_86 \
-	-gencode=arch=compute_86,code=sm_86
+	-fopenmp
 
-echo compile  finished!
+echo "compile finished!"
