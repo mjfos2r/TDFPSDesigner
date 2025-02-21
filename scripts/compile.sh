b79deafe54f4869cecf0de9439db06a1d90358bd
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2025-02-21 15:16:50 -0500

    fixed bad path
    Signed-off-by: Michael Foster <50351736+mjfos2r@users.noreply.github.com>

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,21 +2,21 @@
 echo "Currently in: $(pwd)"
 
 nvcc -std=c++11 \
-	-o ../bin/CalDTWDistMatrixMNSigroot \
-	../cuda/CalDTWDistMatrixMNSigroot.cu \
+	-o bin/CalDTWDistMatrixMNSigroot \
+	cuda/CalDTWDistMatrixMNSigroot.cu \
 	-Xcompiler \
 	-fopenmp \
 	-arch=sm_86
 
 nvcc -std=c++11 \
-	-o ../bin/CalDTWDistMatrixMN \
-	../cuda/CalDTWDistMatrixMN.cu \
+	-o bin/CalDTWDistMatrixMN \
+	cuda/CalDTWDistMatrixMN.cu \
 	-Xcompiler \
 	-fopenmp
 
 nvcc -std=c++11 \
-	-o ../bin/FpsCudaDTWThreshold \
-	../cuda/FpsCudaDTWThreshold.cu \
+	-o bin/FpsCudaDTWThreshold \
+	cuda/FpsCudaDTWThreshold.cu \
 	-Xcompiler \
 	-fopenmp
 
