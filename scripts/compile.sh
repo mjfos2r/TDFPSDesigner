64f7df28b178e77ee4b3bbe471e73be560dac824
Michael J. Foster <50351736+mjfos2r@users.noreply.github.com>
2025-02-21 16:41:05 +0000

    did some organization. moved all cuda source to ./cuda and the compilation script to ./scripts

---
+++ scripts/compile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+nvcc -std=c++11 -o ../bin/CalDTWDistMatrixMNSigroot ../cuda/CalDTWDistMatrixMNSigroot.cu -Xcompiler -fopenmp
+
+nvcc -std=c++11 -o ../bin/CalDTWDistMatrixMN ../cuda/CalDTWDistMatrixMN.cu -Xcompiler -fopenmp
+
+nvcc -std=c++11 -o ../bin/FpsCudaDTWThreshold ../cuda/FpsCudaDTWThreshold.cu -Xcompiler -fopenmp
+
+echo compile  finished!
